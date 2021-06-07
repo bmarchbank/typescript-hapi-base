e4d65900e8cfb3587ae09c2585d91a844b8c6b7a
Benjamin <benjaminmarchbank@gmail.com>
2021-06-07 18:57:15 -0300

    feat(config): add support for server configuration

---
+++ src/config.ts
@@ -0,0 +1,26 @@
+import * as HapiPino from "hapi-pino";
+import { RegisterOptions } from "hapi-swagger";
+import * as pack from '../package.json';
+import * as _ from 'lodash';
+
+export default {
+    server: {
+        port: process.env.SERVER_PORT || 3000,
+        host: process.env.SERVER_HOST || 'localhost',
+    },
+    plugins: {
+        pino: <HapiPino.Options>{
+            level: process.env.PLUGINS_PINO_LOG_LEVEL || 'warn',
+            prettyPrint: !!process.env.PLUGINS_PINO_PRETTY_PRINT,
+            redact: ['req.headers.authorization']
+        },
+        swagger: <RegisterOptions>{
+            info: {
+                // default to the package.json name in Start Case
+                title: process.env.PLUGINS_SWAGGER_INFO_TITLE || _.startCase(pack.name),
+                version: pack.version,
+            }
+            
+        }
+    }
+}
