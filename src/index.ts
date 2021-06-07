6db0b0a052d40fce4252e48658e7759403ae5177
Benjamin <benjaminmarchbank@gmail.com>
2021-06-07 18:57:41 -0300

    feat(index): add initial server start-up functionality

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log('hello world');
+// use dotenv to support ".env" files
+import {config as dotenv} from 'dotenv';
+dotenv();
+
+import * as Hapi from '@hapi/hapi';
+import config from './config';
+import { getAllRoutes } from './routes';
+import plugins from './plugins';
+
+const init = async () => {
+
+    // initialize the server
+    const server = Hapi.server({
+        port: config.server.port,
+        host: config.server.host,
+    });
+
+    // register plugins
+    await server.register(plugins);
+    server.logger.info(`Successfully registered ${plugins.length} plugin(s).`)
+
+    // register all API routes
+    const routes = await getAllRoutes()
+    server.route(routes);
+    server.logger.info(`Successfully registered ${routes.length} route(s).`);
+
+    // start the HAPI server
+    await server.start();
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
